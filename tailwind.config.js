8c99e84a9dac7753ccb8fa93a3771ad1573f14a1
Noam Zaks <noamzaks@outlook.com>
2021-02-03 14:24:50 +0200

    Added TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      "./**/*.html",
+    ]
+  },
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
