4806269e9a7066649b425b45ce8c9172a518558b
Noam Zaks <imnoamzaks@gmail.com>
2024-10-18 19:33:47 +0300

    add markdown heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,32 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import astroOGImage from "./extensions/astroOGImage";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://noamzaks.com",
     integrations: [sitemap(), astroOGImage()],
+    markdown: {
+        rehypePlugins: [
+            rehypeHeadingIds,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    content: {
+                        type: "element",
+                        tagName: "i",
+                        properties: {
+                            className: ["fa-solid", "fa-link", "heading-link"],
+                        },
+                        children: [],
+                    },
+                },
+            ],
+        ],
+    },
+    prefetch: {
+        defaultStrategy: "viewport",
+    },
 });
