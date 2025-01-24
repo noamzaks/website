c77fe1719b4f1efa12887cac779e6f7d10ffe3aa
Noam Zaks <noam@noamzaks.com>
2025-01-24 12:11:44 +0200

    update astro

--- extensions/astroOGImage.ts
+++ extensions/astroOGImage.ts
@@ -1,8 +1,9 @@
 // Based on https://github.com/tomaskebrle/astro-og-image
 
 import type { AstroIntegration } from "astro";
-import * as fs from "fs";
-import { fileURLToPath } from "node:url";
+import { readdirSync, existsSync, mkdirSync, readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { join } from "path";
 import { relative, resolve } from "path";
 import puppeteer from "puppeteer";
 
@@ -10,14 +11,15 @@ export default function astroOGImage(): AstroIntegration {
     return {
         name: "astro-og-image",
         hooks: {
-            "astro:build:done": async ({ dir, routes }) => {
-                // Filters all the routes that need OG image
-                let filteredRoutes = routes;
+            "astro:build:done": async ({ dir }) => {
+                let files = readdirSync(dir, { recursive: true })
+                    .map((x) => join(fileURLToPath(dir), x.toString()))
+                    .filter((x) => x.endsWith(".html"));
 
                 // Creates a directory for the images if it doesn't exist already
                 let directory = fileURLToPath(new URL(`./thumbnails`, dir));
-                if (!fs.existsSync(directory)) {
-                    fs.mkdirSync(directory, { recursive: true });
+                if (!existsSync(directory)) {
+                    mkdirSync(directory, { recursive: true });
                 }
 
                 const browser = await puppeteer.launch({
@@ -25,33 +27,27 @@ export default function astroOGImage(): AstroIntegration {
                 });
 
                 const promises: Promise<void>[] = [];
-                for (const route of filteredRoutes) {
+                for (const f of files) {
                     // Gets the title
-                    const pathname = relative(
-                        "./dist",
-                        route?.distURL?.pathname ?? ""
-                    );
-                    // Skip URLs that have not been built (draft: true, etc.)
-                    if (!pathname) {
+                    const pathname = relative("./dist", f ?? "");
+
+                    const data = readFileSync(f, "utf-8");
+                    let title = ((await data.match(
+                        /<title[^>]*>([^<]+)<\/title>/
+                    )) ?? [])[1];
+                    let description = ((await data.match(
+                        /<meta name="description" content="(.*?)">/
+                    )) ?? [])[1];
+                    let icon = ((await data.match(
+                        /<meta name="thumbnail-icon" content="(.*?)">/
+                    )) ?? [])[1];
+
+                    if (!title || !description || !icon) {
                         continue;
                     }
-                    const data = fs.readFileSync(
-                        route!.distURL!.pathname as any,
-                        "utf-8"
-                    ) as any;
-                    let title = await data.match(
-                        /<title[^>]*>([^<]+)<\/title>/
-                    )[1];
-                    let description = await data.match(
-                        /<meta name="description" content="(.*?)">/
-                    )[1];
-                    let icon = await data.match(
-                        /<meta name="thumbnail-icon" content="(.*?)">/
-                    )[1];
 
                     // Get the html
-                    const html = fs
-                        .readFileSync("og-image.html", "utf-8")
+                    const html = readFileSync("og-image.html", "utf-8")
                         .toString()
                         .replace("@curdir", resolve("."))
                         .replace("@title", title.split(" | ")[0])
@@ -66,7 +62,7 @@ export default function astroOGImage(): AstroIntegration {
                             height: 630,
                         });
 
-                        const split = pathname.split("/");
+                        const split = f.split("/");
                         const filename = split[split.length - 1];
                         const directoryPath = relative(
                             ".",
@@ -83,8 +79,10 @@ export default function astroOGImage(): AstroIntegration {
                                 )
                             )
                         );
-                        if (!fs.existsSync(directoryPath)) {
-                            fs.mkdirSync(directoryPath, { recursive: true });
+                        if (!existsSync(directoryPath)) {
+                            mkdirSync(directoryPath, {
+                                recursive: true,
+                            });
                         }
 
                         await page.screenshot({
